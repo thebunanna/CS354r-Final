bf8b679567af2913427e5458eb08eb2daba9308e
Daniel Hahn <thebunanna@gmail.com>
2020-12-07 13:40:42 -0600

    fixes to hp bar and tool generation

---
+++ final-project/src/gdlibgen.h
@@ -0,0 +1,12 @@
+//GENERATED FILE FROM SCONS
+#include "Enemy.h"
+#include "Player.h"
+#include "Map.h"
+#include "UI/HealthBar.h"
+#include "Inventory/Inventory.h"
+#include "Inventory/InvSlot.h"
+#include "Inventory/ItemTextures.h"
+#include "Inventory/Items/Consumables.h"
+#include "Inventory/Items/InvItem.h"
+#include "Entities/Chest.h"
+void registerblob ();
